eb36c4ab1cb17461a8425d38faee1af5d009e210
vijaycham <104141481+vijaycham@users.noreply.github.com>
2025-01-27 22:16:25 +0530

    connected with mongoDB

--- database.js
+++ database.js
@@ -1,4 +1,20 @@
-//Go
+const { MongoClient } = require("mongodb");
 
-const URI =
+const url =
   "mongodb+srv://NamasteNode:rk3ZeCfiJ8BqauPV@namastenode.tzorr.mongodb.net/";
+const client = new MongoClient(url);
+
+const dbName = "HelloWorld";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("user");
+  return "done.";
+}
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
