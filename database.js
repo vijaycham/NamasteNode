707fe6318dd4129ae75b5bafb36c2edf002d4b5f
vijaycham <104141481+vijaycham@users.noreply.github.com>
2025-01-28 23:50:01 +0530

    Played with DB

--- database.js
+++ database.js
@@ -12,6 +12,41 @@ async function main() {
   console.log("Connected successfully to server");
   const db = client.db(dbName);
   const collection = db.collection("user");
+
+  //Read
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
+
+  // Index
+  // const indexName = await collection.createIndex({ firstName: "Yajiv" });
+  // console.log("index name =", indexName);
+
+  //update
+  // const updateResult = await collection.updateOne(
+  //   { firstName: "Yajiv" },
+  //   { $set: { firstName: "Ajith" } }
+  // );
+  // console.log("Updated documents =>", updateResult);
+
+  //Insert
+  // const insertResult = await collection.insertMany([
+  //   {
+  //     firstName: "Yajiv",
+  //     lastName: "S",
+  //     city: "Chennai",
+  //     PhoneNumber: "88888888888",
+  //   },
+  // ]);
+  // console.log("Inserted documents =>", insertResult);
+
+  //DELETE
+  //
+  // const deleteResult = await collection.deleteMany({
+  //   PhoneNumber: "88888888888",
+  // });
+  // console.log("Deleted documents =>", deleteResult);
+  const countResult = await collection.countDocuments({});
+  console.log("count of documents in the user collection : ", countResult)
   return "done.";
 }
 main()
